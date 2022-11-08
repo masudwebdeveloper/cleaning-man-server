0d972d941af5bc6d3c40662c100ed953815872cb
masudrana5 <abdullainfo30@gmail.com>
2022-11-08 13:21:33 +0600

    review api create done and insert db

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
@@ -12,11 +12,11 @@ app.use(express.json());
 const services = require('./services.json')
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jfl1bty.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
    try {
       const servicesCollection = client.db('cleanerDBUser').collection('services');
+      const reviewsCollection = client.db('cleanerDBUser').collection('reviews');
       app.get('/services', async (req, res) => {
          const size = parseInt(req.query.size);
          const query = {};
@@ -24,11 +24,25 @@ async function run() {
          const services = await cursor.limit(size).toArray();
          const count = await servicesCollection.estimatedDocumentCount();
          console.log(count);
-      res.send({services, count});
+         res.send({ services, count });
+      })
+
+      app.get('/services/:id', async (req, res) => {
+         const id = req.params.id;
+         const query = { _id: ObjectId(id) }
+         const service = await servicesCollection.findOne(query);
+         res.send(service);
+         console.log(query);
+      })
+
+      app.post('/reviews', async (req, res) => {
+         const review = req.body;
+         const result = await reviewsCollection.insertOne(review);
+         console.log(result);
       })
    }
    finally {
-      
+
    }
 }
 run().catch(err => console.error(err))
