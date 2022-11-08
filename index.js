61900c64adf5a830883dc73bbf9f816499f727db
masudrana5 <abdullainfo30@gmail.com>
2022-11-08 17:19:02 +0600

    email filter query api is done

--- index.js
+++ index.js
@@ -9,10 +9,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const services = require('./services.json')
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jfl1bty.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 async function run() {
    try {
       const servicesCollection = client.db('cleanerDBUser').collection('services');
@@ -38,13 +37,36 @@ async function run() {
       app.post('/reviews', async (req, res) => {
          const review = req.body;
          const result = await reviewsCollection.insertOne(review);
+         res.send(result);
          console.log(result);
+      })
+
+      app.get('/reviews', async (req, res) => {
+         let query = {};
+         if (req.query.email) {
+            query = {
+               email: req.query.email
+            } 
+         }
+         const cursor = reviewsCollection.find(query);
+         const review = await cursor.toArray();
+         res.send(review);
+      })
+
+      app.delete('/reviews/:id', async (req, res) => {
+         const id = req.params.id;
+
+         const query = { _id: ObjectId(id) };
+         const result = await reviewsCollection.deleteOne(query);
+         console.log(result);
+         res.send(result);
       })
    }
    finally {
 
    }
 }
+
 run().catch(err => console.error(err))
 
 app.get('/', (req, res) => {
