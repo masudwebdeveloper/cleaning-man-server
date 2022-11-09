c6db1542cc797ee5cce3289ce793282dfab0ede4
masudrana5 <abdullainfo30@gmail.com>
2022-11-09 17:22:03 +0600

    add steps

---
+++ __steps.js
@@ -0,0 +1,8 @@
+/*
+at first npm init -y
+then download express, dotenv, mongodb, jsonwebtoken, 
+initial point index.js
+then script add "strat": "node index.js"
+and initial require set then ok setup
+add vercel.json config file
+*/
